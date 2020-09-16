d86b5d1cbd38cd033e77ab2fdecf03d66a74f098
zsb87 <zhangshibo87@gmail.com>
2020-09-16 15:14:30 -0500

    init

--- code/Sense2StopSync_real_shift/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# note: to run this bash file with 7 cores in parallel: sh batch_run.sh 7
-python3 src/config.py
-mkdir -p data figures result final
-file_list=task_list_orig_offset.txt
-nworkers=$1
-cat $file_list | xargs -L 1 -P $nworkers -I {} python3 src/main.py {}
-python3 src/summarize.py
